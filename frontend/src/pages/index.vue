1cb554f54dad9f2600ba3058c4896738eb78535f
taku820801 <takuchanpower@gmail.com>
2024-03-17 16:34:46 +0900

    vuetifyのインストール

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>hoge</h1>
+  <v-btn variant="plain"> ログイン</v-btn>
+</template>
---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,14 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    ssr: false,
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
