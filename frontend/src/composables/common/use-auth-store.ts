06b57a67d27b99409b40403e30de2090aa99b011
taku820801 <takuchanpower@gmail.com>
2024-05-05 13:47:04 +0900

    自動ログイン

--- frontend/src/composables/common/use-auth-store.ts
+++ frontend/src/composables/common/use-auth-store.ts
@@ -9,6 +9,7 @@ export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
     isAuthenticated: false,
+    token: "",
     user: null as User | null,
     form: {
       email: "",
@@ -24,32 +25,24 @@ export const useAuthStore = defineStore({
     // authedUser: (state) => state.user as User,
   },
   actions: {
-    // authenticate() {
-    //   const token = useCookie("token").value;
-    //   this.isAuthenticated = !!token;
-    // },
-    logout() {
-      useCookie("token").value = null;
-      this.isAuthenticated = false;
-      const router = useRouter();
-      router.push("/");
-    },
+    /**
+     * ログイン処理
+     */
     async login(postData: login) {
       try {
         const hostURL = apiBaseUrl();
-        const response: AxiosResponse = await axios.post(hostURL + "/api_token_auth/", postData);
+        const response: AxiosResponse = await axios.post(hostURL + "api_token_auth/", postData);
         if (response.status === 200) {
-          const token: string = response.data.auth_token;
+          this.token = response.data.auth_token;
           useCookie("token", {
             secure: true,
             maxAge: 86400,
-          }).value = token;
+          }).value = this.token;
           this.isAuthenticated = true;
           this.dialog = true;
-          this.form.email = "";
+          this.getUserInfo();
           return true;
         } else {
-          // エラー時はcatchに飛ぶため、ここは通らないが念の為falseを返す様に設定
           this.isAuthenticated = false;
           this.error.isError = true;
           this.error.errorMessage = "ログインに失敗しました。パスワードとメールアドレスを確認してください。";
@@ -69,6 +62,61 @@ export const useAuthStore = defineStore({
         return false;
       } finally {
         this.form.password = "";
+      }
+    },
+
+    /**
+     * ログアウト処理
+     */
+    logout() {
+      useCookie("token").value = null;
+      this.isAuthenticated = false;
+      this.token = "";
+      this.user = null;
+      this.form.email = "";
+      this.form.password = "";
+    },
+
+    /**
+     * ユーザー情報取得処理
+     *
+     * (取得できればログイン、tokenに問題がありユーザー情報が取得できない場合はログアウトになる）
+     */
+    async getUserInfo() {
+      try {
+        const hostURL = apiBaseUrl();
+        const response: AxiosResponse = await axios.get(hostURL + "api/auth/users/me/", {
+          headers: {
+            Authorization: "Token " + this.token,
+          },
+        });
+        if (response.status === 200) {
+          this.user = response.data;
+          console.log("User info:", this.user);
+          return true;
+        } else {
+          this.logout();
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          await handleErrorResponse(axiosError.response);
+        } else {
+          console.error("getUserInfo error:", axiosError.message);
+        }
+        this.logout();
+      }
+    },
+
+    /**
+     * Cookieからトークンを取得してユーザー情報を取得
+     */
+    async authenticateFromCookie() {
+      const token = await useCookie("token").value;
+      if (token) {
+        this.token = token;
+        this.isAuthenticated = true;
+        await this.getUserInfo();
       }
     },
   },
--- frontend/src/middleware/auth-middleware.global.ts
+++ frontend/src/middleware/auth-middleware.global.ts
@@ -2,15 +2,9 @@ import { useAuthStore } from "~/composables/common/use-auth-store";
 
 export default defineNuxtRouteMiddleware((to, _from) => {
   const authStore = useAuthStore();
-  const noAuthRequired = ["/", "/auth/login"];
 
-  // 認証が不要なページのチェック
-  if (noAuthRequired.includes(to.path)) {
-    return;
-  }
-
-  // 認証が必要なページで認証されていなければログインページへ
+  // オートログイン処理
   if (!authStore.isAuthenticated) {
-    return navigateTo("/auth/login");
+    authStore.authenticateFromCookie();
   }
 });
