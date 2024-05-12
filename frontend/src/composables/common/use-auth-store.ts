328534a3cdc4a4b44fa5402e6ecee04f2bddd06e
taku820801 <takuchanpower@gmail.com>
2024-05-12 23:12:19 +0900

    パスワード変更など色々追加

--- frontend/src/composables/common/use-auth-store.ts
+++ frontend/src/composables/common/use-auth-store.ts
@@ -139,5 +139,83 @@ export const useAuthStore = defineStore({
         }
       }
     },
+
+    /**
+     * パスワード変更処理
+     * TODO 動作確認未実施
+     */
+    async setPassword(new_password: string, re_new_password: string, current_password: string) {
+      try {
+        const hostURL = apiBaseUrl();
+        const response: AxiosResponse = await axios.post(
+          hostURL + "api/auth/users/set_password/",
+          {
+            new_password: new_password,
+            re_new_password: re_new_password,
+            current_password: current_password,
+          },
+          {
+            headers: {
+              Authorization: "Token " + this.token,
+            },
+          }
+        );
+        if (response.status === 200) {
+          this.dialog = true;
+        } else {
+          this.error.isError = true;
+          this.error.errorMessage = "パスワード変更に失敗しました。";
+          console.error("Set password failed");
+        }
+      } catch (error) {
+        this.error.isError = true;
+        this.error.errorMessage = "パスワード変更に失敗しました。";
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          await handleErrorResponse(axiosError.response);
+        } else {
+          console.error("An error occurred:", axiosError.message);
+        }
+      }
+    },
+
+    /**
+     * メールアドレス変更処理
+     * TODO 動作確認未実施
+     */
+    async setEmail(current_password: string, new_email: string, re_new_email: string) {
+      try {
+        const hostURL = apiBaseUrl();
+        const response: AxiosResponse = await axios.post(
+          hostURL + "api/auth/users/set_email/",
+          {
+            new_email: new_email,
+            re_new_email: re_new_email,
+            current_password: current_password,
+          },
+          {
+            headers: {
+              Authorization: "Token " + this.token,
+            },
+          }
+        );
+        if (response.status === 200) {
+          this.dialog = true;
+        } else {
+          this.error.isError = true;
+          this.error.errorMessage = "メールアドレス変更に失敗しました。";
+          console.error("Set email failed");
+        }
+      } catch (error) {
+        this.error.isError = true;
+        this.error.errorMessage = "メールアドレス変更に失敗しました。";
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          await handleErrorResponse(axiosError.response);
+        } else {
+          console.error("An error occurred:", axiosError.message);
+        }
+      }
+    },
   },
 });
