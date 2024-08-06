54ec02f0d3f52b26a776713530c3eb2d9c7dd9ef
takushisato <takuchanpower@gmail.com>
2024-08-06 23:23:54 +0900

    password set 追加

---
+++ frontend/src/components/settings/auth/PasswordSetForm.vue
@@ -0,0 +1,8 @@
+<template>
+    <p>password set</p>
+</template>
+<script lang="ts">
+export default defineComponent({
+    name: "PasswordSetForm",
+});
+</script>
---
+++ frontend/src/pages/settings/auth/set-password.vue
@@ -0,0 +1,10 @@
+<template>
+    <PasswordSetForm />
+</template>
+<script lang="ts">
+import PasswordSetForm from "@/components/settings/auth/PasswordSetForm.vue";
+export default defineComponent({
+    components: { PasswordSetForm },
+    name: "set-password",
+});
+</script>
