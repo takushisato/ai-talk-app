651460921dbcab3e666b420faab44e99304f8a65
takushisato <takuchanpower@gmail.com>
2024-08-08 23:13:46 +0900

    set-password ページを埋め込み

--- frontend/src/components/settings/SettingIndexPage.vue
+++ frontend/src/components/settings/SettingIndexPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="text-center w-100 mt-8">
-    <nuxt-link to="/settings/auth">ユーザー情報変更</nuxt-link>
-    <br />
-    <br />
-    <nuxt-link to="/">AI設定変更</nuxt-link>
-  </div>
+    <div class="text-center w-100 mt-8">
+        <nuxt-link to="/settings/auth">ユーザー情報変更</nuxt-link>
+        <br />
+        <br />
+        <nuxt-link to="/">AI設定変更</nuxt-link>
+    </div>
 </template>
 <script lang="ts">
 export default defineComponent({
-  components: {},
-  name: "settingIndexPage",
+    components: {},
+    name: 'settingIndexPage',
 });
 </script>
--- frontend/src/components/settings/auth/SettingAuthPage.vue
+++ frontend/src/components/settings/auth/SettingAuthPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="text-center w-100 mt-8">
-    <nuxt-link to="/">メールアドレス変更</nuxt-link>
-    <br />
-    <br />
-    <nuxt-link to="/">パスワード変更</nuxt-link>
-  </div>
+    <div class="text-center w-100 mt-8">
+        <nuxt-link to="/">メールアドレス変更</nuxt-link>
+        <br />
+        <br />
+        <nuxt-link to="/settings/auth/set-password/">パスワード変更</nuxt-link>
+    </div>
 </template>
 <script lang="ts">
 export default defineComponent({
-  components: {},
-  name: "SettingAuthPage",
+    components: {},
+    name: 'SettingAuthPage',
 });
 </script>
