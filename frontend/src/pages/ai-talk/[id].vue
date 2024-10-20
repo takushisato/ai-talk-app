609e4a4ea3216e7becab7776196832a74b2aa38e
takushisato <takuchanpower@gmail.com>
2024-10-20 17:24:03 +0900

    ai talkのidページを追加

---
+++ frontend/src/pages/ai-talk/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <ServiceTalkForm :id="id" />
+</template>
+<script lang="ts">
+export default {
+    name: 'talks',
+};
+</script>
+<script setup lang="ts">
+/**
+ * URLからidを取得してコンポーネントに渡し
+ */
+const route = useRoute();
+const { id } = route.params;
+</script>
+<style scoped></style>
